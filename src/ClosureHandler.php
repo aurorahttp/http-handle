1738e49e154cc59bb3415839ef478379f41de502
panlatent <panlatent@gmail.com>
2018-01-23 17:22:20 +0800

    Remove handle method optional argument

--- src/ClosureHandler.php
+++ src/ClosureHandler.php
@@ -2,26 +2,46 @@
 
 namespace Aurora\Http\Handler;
 
+use Aurora\Http\Handler\Bundle\ListBundle;
+
 class ClosureHandler implements HandlerInterface
 {
+    /**
+     * @var callable
+     */
     protected $callback;
 
+    /**
+     * ClosureHandler constructor.
+     *
+     * @param callable $callback
+     */
     public function __construct(callable $callback)
     {
         $this->callback = $callback;
     }
 
+    /**
+     * @param callable $callback
+     * @return static
+     */
     public static function create(callable $callback)
     {
         return new static($callback);
     }
 
-    public function handle($request, HandlerInterface $next = null)
+    /**
+     * @param mixed            $request
+     * @param HandlerInterface $next
+     * @return mixed
+     */
+    public function handle($request, HandlerInterface $next)
     {
-        if ($next === null) {
-            return call_user_func($this->callback, $request, $next);
+        if (! $next instanceof BundleInterface) {
+            $next = new ListBundle();
+            $next->backPush($next);
         }
 
-        return $next->handle(call_user_func($this->callback, $request, $next), $next);
+        return call_user_func($this->callback, $request, $next);
     }
 }
--- tests/ClosureHandlerTest.php
+++ tests/ClosureHandlerTest.php
@@ -3,6 +3,7 @@
 namespace Aurora\Http\Handler\Tests;
 
 use Aurora\Http\Handler\ClosureHandler;
+use Aurora\Http\Handler\HandlerInterface;
 use PHPUnit\Framework\TestCase;
 
 class ClosureHandlerTest extends TestCase
@@ -12,7 +13,11 @@ class ClosureHandlerTest extends TestCase
         $handler = new ClosureHandler(function($request) {
            return 'A and ' . $request;
         });
+        $this->assertEquals('A and B', $handler->handle('B', new Handler()));
 
-        $this->assertEquals('A and B', $handler->handle('B'));
+        $handler = new ClosureHandler(function($request, HandlerInterface $next) {
+            return $next->handle('A and ' . $request, $next);
+        });
+        $this->assertEquals('A and B', $handler->handle('B', new Handler()));
     }
 }
